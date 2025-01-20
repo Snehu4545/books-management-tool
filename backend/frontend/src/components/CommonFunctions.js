25e4234ad7d8f75ce72dd98318b8511235c9dbec
SnehA Yadav <snehayadav4464@gmail.com>
2025-01-20 21:55:28 +0530

    Delete backend/frontend directory

--- backend/frontend/src/components/CommonFunctions.js
+++
@@ -1,9 +0,0 @@
-export const getBookTypeName = (typeId, bookTypes) => {
-    const type = bookTypes?.find(type => type.id === typeId);
-    return type ? type.type : "--";
-};
-
-export const getGenreName = (genreId, genres) => {
-    const genre = genres?.find(genre => genre.id === genreId);
-    return genre ? genre.name : "--";
-};
